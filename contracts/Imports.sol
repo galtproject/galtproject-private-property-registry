7623b191e8e2c3d3ceea7048ede372d8985d7564
Nikita Chebykin <nikita@chebyk.in>
2019-11-14 21:11:34 +0200

    Cleanup imports

--- contracts/Imports.sol
+++ contracts/Imports.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.5.10;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
-//import "@galtproject/core/contracts/reputation/interfaces/IRA.sol";
 
 
 contract Imports {
