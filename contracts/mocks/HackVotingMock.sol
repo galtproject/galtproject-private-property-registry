7f1b088b869ae305c5c7b3cd90e9e58cb5ca7bbd
jonybang <Jonybange@gmail.com>
2020-03-16 23:39:26 +0200

    try to hack locker

---
+++ contracts/mocks/HackVotingMock.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.5.13;
+
+import "../interfaces/IPPToken.sol";
+
+contract HackVotingMock {
+  event PreCall(bytes data);
+  event Result(bool status, bytes response);
+
+  address public registry;
+
+  constructor(address _registry) public {
+    registry = _registry;
+  }
+
+  function voteByTokens(
+    uint256[] calldata _tokenIds,
+    uint256 _voteId,
+    bool _support,
+    bool _executesIfDecided
+  )
+   external
+  {
+    bytes4 methodSelector = IPPToken(0).transferFrom.selector;
+    bytes memory _data = abi.encodeWithSelector(methodSelector, msg.sender, address(this), _tokenIds[0]);
+    emit PreCall(_data);
+    (bool status, bytes memory response) = registry.delegatecall(_data);
+    emit Result(status, response);
+  }
+}
